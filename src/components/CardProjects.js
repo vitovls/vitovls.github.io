6388d5fa4b170050fd78320459f64e47fbb39366
vitovls <vitovls37@gmail.com>
2022-04-20 18:36:13 -0300

    refactor: Mudando CSS do Project Cards para arquivos certos

--- src/components/CardProjects.js
+++ src/components/CardProjects.js
@@ -2,25 +2,26 @@ import Link from 'next/link'
 import react from 'react'
 import listProjects from '../list-projects.json'
 import Tag from '../components/Tags'
+import Image from 'next/image'
 
 export default function CardProjects({props}) {
   return (
-    listProjects.map((project) => {
+    listProjects.map((project, index) => {
       return (
-        <>
-          <a target='_blank' href={`${project.gitHubUrl}`}>
-            <section className='container-project-card'>
-              <img className='image-project-card' src={`${project.imageUrl}`} /> 
+        <section key={`Projeto Nº ${index}`} className='container-project-card'> 
+          <section>
+            <a target='_blank' href={`${project.gitHubUrl}`} rel="noreferrer">
+              <Image width='350px' height='230px' alt={`Miniatura do Projeto: ${project.name}`} className='image-project-card' src={project.imageUrl}/>
               <article className='description-project-card'>
                 <h1 className='headling-project-card'>{project.name}</h1>
                 <p className='paragraph-project-card'>{project.about}</p>
                 <section className='section-tags-project-card'>
-                  {project.stacks.map((stacks) => <Tag nameTag={stacks.stackName} colorTag={stacks.brandColor}/>)}
+                  {project.stacks.map((stacks, index) => <Tag key={`Stack Nº ${index}`} nameTag={stacks.stackName} colorTag={stacks.brandColor}/>)}
                 </section>
               </article>
-            </section>
-          </a>
-        </>
+            </a>
+          </section>
+          </section>
       )
     })
   )

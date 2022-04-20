cba59a418a7a2d2cb6d38ef4c1c8643b33e72e79
vitovls <vitovls37@gmail.com>
2022-04-20 19:10:20 -0300

    refactor: Mandando tag image padrão por Image do next no footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,15 @@
+import Image from 'next/image'
 
 export default function Footer() {
   return (
     <footer>
-      <p className='paragraph-footer'>"Feito com Next.JS, muito café e 💙"</p>
+      <p className='paragraph-footer'>{`"Feito com Next.JS, muito café e 💙"`}</p>
       <section className='footer-icons'>
-      <a target='_blank' href='https://www.github.com/vitovls'>
-        <img className='github-icon-footer' src='https://i.imgur.com/7rITkqB.png'/>
+      <a target='_blank' href='https://www.github.com/vitovls' rel="noreferrer">
+        <Image className='github-icon-footer' alt='GitHub Logo' width='50px' height='50px' src='/SocialLogos/Github-Logo.png' />
       </a>
-      <a target='_blank' href='https://www.linkedin.com/in/vitovls'>
-        <img className='linkedin-icon-footer' src='https://i.imgur.com/DhHQ7Y2.png'/>
+      <a target='_blank' href='https://www.linkedin.com/in/vitovls' rel="noreferrer">
+      <Image className='linkedin-icon-footer' alt='Linkedin Logo' width='50px' height='50px' src='/SocialLogos/Linkedin-Logo.png' />
       </a>
       </section>
     </footer>
